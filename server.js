b795800bf4ffef136dcc42be132c8b28e27fbe1c
MathCoder <mathieufc31@gmail.com>
2023-02-19 13:53:25 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
+);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+app.listen(process.env.PORT || 3000);
